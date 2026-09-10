6f72659ed2ba72ce5dd6f309e28441680ba55925
denise <denisiasene2003@gmail.com>
2026-09-10 02:09:54 +0000

    feat: ajouter le routage FastRoute

--- public/index.php
+++ public/index.php
@@ -1,4 +1,71 @@
 <?php
 
-echo "Bonjour Denise !";
-echo "Docker fonctionne sans rebuild !";
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use FastRoute\Dispatcher;
+use function FastRoute\simpleDispatcher;
+
+// Charger les variables d'environnement
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->safeLoad();
+
+// Initialiser la base de données
+require_once dirname(__DIR__) . '/config/database.php';
+
+// Charger le container
+$container = require dirname(__DIR__) . '/config/container.php';
+
+// Charger les routes
+$routes = require dirname(__DIR__) . '/routes/web.php';
+
+// Créer le dispatcher FastRoute
+$dispatcher = simpleDispatcher($routes);
+
+// Récupérer la méthode HTTP et l'URI
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+
+// Dispatcher la requête
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+
+    case Dispatcher::NOT_FOUND:
+        http_response_code(404);
+
+        $view = $container->get(App\View\View::class);
+        $view->render('error/404');
+
+        break;
+
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        http_response_code(405);
+
+        echo 'Méthode HTTP non autorisée.';
+        break;
+
+   case Dispatcher::FOUND:
+
+    $handler = $routeInfo[1];
+    $vars = $routeInfo[2];
+
+    [$controllerClass, $method] = $handler;
+
+    $controller = $container->get($controllerClass);
+
+    $controller->{$method}(
+        ...array_map(
+            static fn ($value) => ctype_digit($value) ? (int) $value : $value,
+            array_values($vars)
+        )
+    );
+
+    break;
+}
+
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Controller\SalleController;
+use App\Controller\ReservationController;
+
+return function (FastRoute\RouteCollector $routes): void {
+
+    $routes->addRoute('GET', '/salles', [SalleController::class, 'index']);
+    $routes->addRoute('GET', '/salles/create', [SalleController::class, 'create']);
+    $routes->addRoute('POST', '/salles', [SalleController::class, 'store']);
+    $routes->addRoute('GET', '/salles/{id:\d+}', [SalleController::class, 'show']);
+    $routes->addRoute('GET', '/salles/{id:\d+}/edit', [SalleController::class, 'edit']);
+    $routes->addRoute('POST', '/salles/{id:\d+}', [SalleController::class, 'update']);
+
+    $routes->addRoute('GET', '/reservations', [ReservationController::class, 'index']);
+    $routes->addRoute('GET', '/reservations/create', [ReservationController::class, 'create']);
+    $routes->addRoute('POST', '/reservations', [ReservationController::class, 'store']);
+    $routes->addRoute('GET', '/reservations/{id:\d+}', [ReservationController::class, 'show']);
+    $routes->addRoute('POST', '/reservations/{id:\d+}/cancel', [ReservationController::class, 'cancel']);
+   
+};
